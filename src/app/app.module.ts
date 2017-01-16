10baa9eb1ab555458a77c2c37aa7e9c4ed16bed9
Nathaniel Fodrea <fodreallc@gmail.com>
2017-01-16 15:06:13 -0500

    Auth Base

---
+++ src/app/app.module.ts
@@ -0,0 +1,71 @@
+import { NgModule, ErrorHandler } from '@angular/core';
+import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
+import { MyApp } from './app.component';
+
+//page imports
+import { AboutPage } from '../pages/about/about';
+import { ContactPage } from '../pages/contact/contact';
+import { HomePage } from '../pages/home/home';
+import { TabsPage } from '../pages/tabs/tabs';
+import { WorkoutsPage } from '../pages/workouts/workouts';
+import { LoginPage } from '../pages/login/login';
+import { SignUpPage } from '../pages/sign-up/sign-up';
+import { ResetPasswordPage } from '../pages/reset-password/reset-password';
+
+//import provider
+import { AuthData } from '../providers/auth-data'
+
+//import AF2
+import { AngularFireModule, AuthProviders, AuthMethods } from 'angularfire2';
+
+// AF2 Settings
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyAFibWvykNa5YhbxIjzv_KSf5UzUgvnjwY",
+  authDomain: "workouttracker-43547.firebaseapp.com",
+  databaseURL: "https://workouttracker-43547.firebaseio.com",
+  storageBucket: "workouttracker-43547.appspot.com",
+  messagingSenderId: "304456866072"
+};
+
+const myFirebaseAuthConfig = {
+  provider: AuthProviders.Password,
+  method: AuthMethods.Password
+}
+
+
+@NgModule({
+  declarations: [
+    MyApp,
+    AboutPage,
+    ContactPage,
+    HomePage,
+    TabsPage,
+    WorkoutsPage,
+    LoginPage,
+    SignUpPage,
+    ResetPasswordPage
+  ],
+  imports: [
+    IonicModule.forRoot(MyApp),
+    AngularFireModule.initializeApp(firebaseConfig, myFirebaseAuthConfig)
+  ],
+  bootstrap: [IonicApp],
+  entryComponents: [
+    MyApp,
+    AboutPage,
+    ContactPage,
+    HomePage,
+    TabsPage,
+    WorkoutsPage,
+    LoginPage,
+    SignUpPage,
+    ResetPasswordPage
+  ],
+  providers: [
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    AuthData
+  ]
+})
+export class AppModule {}
+
